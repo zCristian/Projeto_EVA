e38dde6821def337a5704b1d9876e68da5c31221
vinicius-souza <vinicius.soaressouza@outlook.com>
2026-04-18 20:04:56 -0200

    atualizacao W0

--- equations.py
+++ equations.py
@@ -7,10 +7,10 @@ Created on Sat Mar 14 14:46:03 2026
 
 import numpy as np
 from scipy.optimize import fsolve
-
 from parameters import *
 
 results = Estimatives()
+results_updated = Estimatives_updated()
 AR = AspectRatio()
 
 ################## FUEL EQUATIONS ##################
@@ -59,7 +59,6 @@ def WAR():
 
 # L/Dmax Equation
 def LDmax():  
-    print()  
     LDmax = Kld * np.sqrt(AR.value / (SwetSref))  
     results.ldmax = LDmax
     return LDmax
@@ -77,3 +76,35 @@ def Cc():
     cc = Ccbhp * V / (550 * Ccnp)
     results.Cc = cc
     return cc
+
+################## UPDATED EQUATIONS ################## 
+def updated_WeW0(W0):
+    wew0 = -0.25 + 1.18 * (W0 ** -0.2) * (A ** 0.08) * (hpW0 ** 0.05) * (W0S ** -0.05) * (Vmax ** 0.27)
+    results_updated.wew0 = wew0
+    return wew0
+
+def updated_W2W1():
+    w2w1 = 1.0065 - 0.0325 * M
+    #w2w1 = 0.985
+    results_updated.w2w1 = w2w1
+    return w2w1
+
+def updated_Wf(W1, W2):
+    Wf = 1.06 * ((1 - updated_W2W1()) * W1 + (1 - Wcruise()) * W2)
+    results_updated.wf = Wf
+    results_updated.wfw0 = Wf/W1
+    return Wf
+
+# Iterative solution for W0
+def func_updated(W0):
+    W1 = W0
+    W2 = updated_W2W1() * W1
+    W3 = Wcruise() * W2
+    We = updated_WeW0(W0) * W0
+    results_updated.wcruise = Wcruise()
+
+    results_updated.w2 = W2
+    results_updated.we = We
+    results_updated.w3 = W3
+
+    return W0 - Wcrew - Wpay - updated_Wf(W1, W2) - updated_WeW0(W0) * W0
--- parameters.py
+++ parameters.py
@@ -11,7 +11,6 @@ Wcrew = 0 # Wcrew
 Wpay = 60 * 2.2046 # Wpayload (lb)
 W1W0 = 0.97 # Warmup and takeoff
 W2W1 = 0.985 # Climb
-#W5W4 = 0.995 # Land
 
 # Aircraft parameters
 A = 1.67 # Table 3.1 Raymer
@@ -26,7 +25,14 @@ Ccnp = 0.8  # Propeller cruise Np - Table 3.4
 # Mission parameters
 R = 2000 * 3280.8399 # Range (ft)
 V = 250 * 0.91134 # Cruise velocity (ft/s)
+Vsound = np.sqrt(1.4 * 287 * 275) * 3.28084 # Sound velocity (ft/s)
+M = V / Vsound
 Hc = 2000 * 3.28084 # Teto de serviço (ft)
+
+# Calculated values
+hpW0 = 0.046
+W0S = 20
+Vmax = 280 * 0.539 # kt
 
 class AspectRatio:
     value = float
@@ -38,7 +44,12 @@ class Estimatives:
     wfw0 = float
     ldc = float
     ldmax = float
-    w0 = float 
+    w0 = float
+    w1 = float
+    w2 = float
+    w3 = float
+    wf = float
+    we = float 
     Cc = float
 
     def __str__(self):
@@ -50,7 +61,34 @@ class Estimatives:
             f"ldc: {self.ldc}\n"
             f"ldmax: {self.ldmax}\n"
             f"w0: {self.w0}\n"
+            f"w1: {self.w1}\n"
+            f"w2: {self.w2}\n"
+            f"w3: {self.w3}\n"
+            f"wf: {self.wf}\n"
+            f"we: {self.we}\n"
             f"Cc: {self.Cc}"
         )
 
+class Estimatives_updated:
+    w2w1 = float
+    wcruise = float
+    wew0 = float
+    wfw0 = float
+    wf = float
+    w0 = float
+    w2 = float
+    w3 = float
+    we = float 
 
+    def __str__(self):
+        return (
+            f"w2w1: {self.w2w1}\n"
+            f"wcruise: {self.wcruise}\n"
+            f"wew0: {self.wew0}\n"
+            f"wfw0: {self.wfw0}\n"
+            f"wf: {self.wf}\n"
+            f"w0: {self.w0}\n"
+            f"w2: {self.w2}\n"
+            f"w3: {self.w3}\n"
+            f"we: {self.we}"
+        )
